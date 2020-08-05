90621faf96c5cf224a60363810d563da3c94f6ca
Craig Reynolds <cwr@red3d.com>
2020-08-05 15:37:19 -0700

    First commit from local repository to GitHub.
    Add Xcode project, docs directory, .gitignore, and main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LazyPredator
+//
+//  Created by Craig Reynolds on 8/5/20.
+//  Copyright © 2020 Craig Reynolds. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
